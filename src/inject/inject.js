6cbfefdee91ff6257c70d11860b2acc0aae67893
biskazz <biser.p.atanasov@gmail.com>
2017-07-05 15:35:07 +0300

    1.0.0

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,9 +1,19 @@
 // Yes it's very complicated I know.
 
 var actualCodeToInject = '(' + function () {
-  window.Suls.SmoothScroller = {}
+  let uDontScroll = function () {
+    console.log('nema da scrollvash')
+  }
+  window.Suls.SmoothScroller.scrollToElement = uDontScroll
+  window.Suls.SmoothScroller.scrollToElementBottom = uDontScroll
+  window.Suls.SmoothScroller.scrollToPageHash = uDontScroll
+  window.Suls.SmoothScroller.scrollToPageHashAndOpenAccordion = uDontScroll
 } + ')();'
+
 var scriptToInject = document.createElement('script')
+
 scriptToInject.textContent = actualCodeToInject;
+
 (document.head || document.documentElement).appendChild(scriptToInject)
+
 scriptToInject.remove()
